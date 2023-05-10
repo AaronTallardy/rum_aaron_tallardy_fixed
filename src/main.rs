bc4339508f195f6ba1533444206e198bb7100a3e
AaronTallardy <90728260+AaronTallardy@users.noreply.github.com>
2023-05-10 19:50:14 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,35 @@ fn main() {
         
         
 }
-
 pub fn load(input: Option<&str>) -> Vec<u32> {
+    let mut buf_reader: Box<dyn std::io::BufRead> = match input {
+        None => Box::new(std::io::BufReader::new(std::io::stdin())),
+        Some(filename) => Box::new(std::io::BufReader::new(
+            std::fs::File::open(filename).unwrap(),
+        )),
+    };
+
+    let mut instructions = Vec::new();
+    loop {
+        let mut chunk = [0; 4];
+        match buf_reader.read_exact(&mut chunk) {
+            Ok(()) => {
+                instructions.push(u32::from_be_bytes(chunk));
+            }
+            Err(e) => {
+                if e.kind() == std::io::ErrorKind::UnexpectedEof {
+                    break;
+                } else {
+                    panic!("Error reading input file: {}", e);
+                }
+            }
+        }
+    }
+
+    instructions
+}
+
+/* pub fn load(input: Option<&str>) -> Vec<u32> {
     let mut raw_reader: Box<dyn std::io::BufRead> = match input {
         None => Box::new(std::io::BufReader::new(std::io::stdin())),
         Some(filename) => Box::new(std::io::BufReader::new(
@@ -33,4 +60,4 @@ pub fn load(input: Option<&str>) -> Vec<u32> {
     .collect();
     instructions
     
-}
+} */
