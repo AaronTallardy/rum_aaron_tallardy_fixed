575c59649454964c67e6ee8cfb913a52daeb2ef7
AaronTallardy <90728260+AaronTallardy@users.noreply.github.com>
2023-05-10 22:04:59 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -25,34 +25,6 @@ pub fn load(input: Option<&str>) -> Vec<u32> {
     };
 
     let mut instructions = Vec::new();
-    let mut buffer = [0u8; 4];
-    loop {
-        match buf_reader.read_exact(&mut buffer) {
-            Ok(()) => {
-                instructions.push(u32::from_be_bytes(buffer));
-            }
-            Err(e) => {
-                if e.kind() == std::io::ErrorKind::UnexpectedEof {
-                    break;
-                } else {
-                    panic!("Error reading input file: {}", e);
-                }
-            }
-        }
-    }
-
-    instructions
-}
-
-/* pub fn load(input: Option<&str>) -> Vec<u32> {
-    let mut buf_reader: Box<dyn std::io::BufRead> = match input {
-        None => Box::new(std::io::BufReader::new(std::io::stdin())),
-        Some(filename) => Box::new(std::io::BufReader::new(
-            std::fs::File::open(filename).unwrap(),
-        )),
-    };
-
-    let mut instructions = Vec::new();
     loop {
         let mut chunk = [0; 4];
         match buf_reader.read_exact(&mut chunk) {
@@ -70,7 +42,7 @@ pub fn load(input: Option<&str>) -> Vec<u32> {
     }
 
     instructions
-} */
+}
 
 /* pub fn load(input: Option<&str>) -> Vec<u32> {
     let mut raw_reader: Box<dyn std::io::BufRead> = match input {
